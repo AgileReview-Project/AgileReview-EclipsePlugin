da64ff6f68681902e5e117527ebc9b22ac4cb9d9
Peter Reuter <peter.reuter@agilereview.org>
2012-01-16 16:15:31 +0000

    finished work on reply pojo
    git-svn-id: http://svn.code.sf.net/p/agilereview/code/AgileReview_project/branches/new_architecture@779 d30e6b9e-d291-474e-934b-121ad53275c3

--- org.agilereview.core/src/org/agilereview/core/external/storage/Comment.java
+++ org.agilereview.core/src/org/agilereview/core/external/storage/Comment.java
@@ -5,6 +5,9 @@ import java.util.Date;
 
 import org.eclipse.core.resources.IFile;
 
+/**
+ * A class that stores date of comments.
+ */
 public class Comment {
 	
 	/**
--- org.agilereview.core/src/org/agilereview/core/external/storage/Reply.java
+++ org.agilereview.core/src/org/agilereview/core/external/storage/Reply.java
@@ -2,11 +2,93 @@ package org.agilereview.core.external.storage;
 
 import java.util.Date;
 
+/**
+ * A class that is used to store replies that were added to a comment.
+ */
 public class Reply {
 	
-	private String author;
-	private Date creationDate;
-	private Date modificationDaten;
-	private String text;
+	/**
+	 * The author of the reply 
+	 */
+	private String author = ""; //TODO maybe seperate author object for sync with color?
+	/**
+	 * The date when the reply was create initially 
+	 */
+	private Date creationDate = new Date();
+	/**
+	 * The date when the reply was modified lastly 
+	 */
+	private Date modificationDate = creationDate;
+	/**
+	 * The text of the reply 
+	 */
+	private String text = "";
+	
+	/**
+	 * Constructor that should be used if a reply is reconstructed from storage
+	 * @param author the author of the reply
+	 * @param creationDate the date when the reply was create initially
+	 * @param modificationDate date when the reply was create lastly
+	 * @param text the text of the reply
+	 */
+	public Reply(String author, Date creationDate, Date modificationDate,
+			String text) {
+		this.author = author;
+		this.creationDate = creationDate;
+		this.modificationDate = modificationDate;
+		this.text = text;
+	}
+	
+	/**
+	 * Constructor that should be used if a new reply is created. 
+	 */
+	public Reply() {
+		//TODO take author from properties
+		this.author = "";
+	}
 
+	/**
+	 * @return the author of the reply
+	 */
+	public String getAuthor() {
+		return author;
+	}
+	
+	/**
+	 * @return the date when the reply was created initially
+	 */
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	
+	/**
+	 * @return the date when the reply was modified lastly
+	 */
+	public Date getModificationDate() {
+		return modificationDate;
+	}
+	
+	/**
+	 * Resets the modification date. Should be used if an attribute was changed
+	 * e.g. in setters. 
+	 */
+	private void resetModificationDate() {
+		this.modificationDate = new Date();
+	}
+	
+	/**
+	 * @return the current text of the reply
+	 */
+	public String getText() {
+		return text;
+	}
+	
+	/**
+	 * @param text the new text of the reply
+	 */
+	public void setText(String text) {
+		this.text = text;
+		resetModificationDate();
+	}
+	
 }
