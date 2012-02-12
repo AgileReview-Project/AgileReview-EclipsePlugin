57c8d8a919f91e81b06183d194e9c3f81e213dfd
Malte Brunnlieb <malte.brunnlieb@agilereview.org>
2012-02-12 20:27:06 +0000

    some tests implemented
    git-svn-id: http://svn.code.sf.net/p/agilereview/code/AgileReview_project/branches/new_architecture@816 d30e6b9e-d291-474e-934b-121ad53275c3

--- org.agilereview.core_Test/src/external/storage/CommentTest.java
+++ org.agilereview.core_Test/src/external/storage/CommentTest.java
@@ -1,9 +1,16 @@
 package external.storage;
 
 import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
 import junit.framework.Assert;
 
 import org.agilereview.core.external.storage.Comment;
+import org.agilereview.core.external.storage.Reply;
+import org.agilereview.core.external.storage.Review;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -14,17 +21,43 @@ public class CommentTest {
 	@Test
 	public void testCommentStringIFile() {
 		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject("org.agilereview.core_Test");
-		if(project != null) {
-			IFile file = project.getFile("resources/Test.txt");
-			Assert.assertNotNull(new Comment("r2.klaus.c4", file));
-		} else {
-			throw new AssertionError("Project org.agilereview.core_Test could not be found", null);
+		if(project == null) throw new AssertionError("Project org.agilereview.core_Test could not be found!", null);
+		
+		//generate comment contents
+		IFile file = project.getFile("resources/Test.txt");
+		//create comment
+		Comment comment = new Comment("r2", file); //TODO check the meaning of this "id" parameter (why is it possible to create a comment only with id and file?)
+		Assert.assertNotNull(comment);
+		
+		//check integrity
+		if(!comment.getId().equals("r2") || !comment.getCommentedFile().equals(file)) {
+			throw new AssertionError("Comment integrity not valid after creation!", null);
 		}
 	}
 
 	@Test
 	public void testCommentStringStringIFileReviewDateDateStringIntIntStringArrayListOfReply() {
-		fail("Not yet implemented"); // TODO
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject("org.agilereview.core_Test");
+		if(project == null) throw new AssertionError("Project org.agilereview.core_Test could not be found!", null);
+		
+		//generate comment contents
+		IFile file = project.getFile("resources/Test.txt");
+		Date d = Calendar.getInstance().getTime();
+		Reply r = new Reply();
+		ArrayList<Reply> replies = new ArrayList<Reply>();
+		replies.add(r);
+		Review review = new Review("r2");
+		//create comment
+		Comment comment = new Comment("c4", "klaus", file, review, d, d, "anyone", 1, 2, "description", replies);
+		Assert.assertNotNull(comment);
+		
+		//check integrity
+		if(!comment.getId().equals("c4") || !comment.getCommentedFile().equals(file) || !comment.getAuthor().equals("klaus")
+				|| !comment.getReview().equals(review) || !comment.getCreationDate().equals(d) || !comment.getModificationDate().equals(d)
+				|| !comment.getRecipient().equals("anyone") || comment.getStatus() != 1 || comment.getPriority() != 2
+				|| !comment.getText().equals("description") || !comment.getReplies().get(0).equals(r)) {
+			throw new AssertionError("Comment integrity not valid after creation!", null);
+		}
 	}
 
 	@Test
