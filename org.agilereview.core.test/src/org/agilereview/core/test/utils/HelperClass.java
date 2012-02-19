35b6e4e546b1ed51a225b842d8edef9d2ee405aa
Malte Brunnlieb <malte.brunnlieb@agilereview.org>
2012-02-19 10:34:29 +0000

    JavaDoc created
    git-svn-id: http://svn.code.sf.net/p/agilereview/code/AgileReview_project/branches/new_architecture@837 d30e6b9e-d291-474e-934b-121ad53275c3

--- org.agilereview.core.test/src/org/agilereview/core/test/utils/HelperClass.java
+++ org.agilereview.core.test/src/org/agilereview/core/test/utils/HelperClass.java
@@ -1,14 +1,32 @@
+/**
+ * Copyright (c) 2011, 2012 AgileReview Development Team and others.
+ * All rights reserved. This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License - v 1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * Contributors: Malte Brunnlieb, Philipp Diebold, Peter Reuter, Thilo Rauch
+ */
 package org.agilereview.core.test.utils;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 
+/**
+ * 
+ * @author Malte Brunnlieb (19.02.2012)
+ */
 public class HelperClass {
-	
-	public static IFile getIFile(String path) {
-		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject("org.agilereview.core_Test");
-		if(project == null) throw new AssertionError("Project org.agilereview.core_Test could not be found!");
-		return project.getFile("resources/Test.txt");
-	}
+
+    /**
+     * Creates an {@link IFile} representation for the given relative path according to this project
+     * @param path relative path according to this project
+     * @return an {@link IFile} representation of the given file path
+     * @author Malte Brunnlieb (19.02.2012)
+     */
+    public static IFile getIFile(String path) {
+	IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject("org.agilereview.core_Test");
+	if (project == null)
+	    throw new AssertionError("Project org.agilereview.core_Test could not be found!");
+	return project.getFile("resources/Test.txt");
+    }
 }
