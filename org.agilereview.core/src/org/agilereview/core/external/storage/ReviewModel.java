6e0a59733189ae7a0da0c10657db7dc4e11c97bb
Peter Reuter <peter.reuter@agilereview.org>
2012-02-19 08:21:44 +0000

    changed license to EPL v1
    git-svn-id: http://svn.code.sf.net/p/agilereview/code/AgileReview_project/branches/new_architecture@834 d30e6b9e-d291-474e-934b-121ad53275c3

--- org.agilereview.core/src/org/agilereview/core/external/storage/ReviewModel.java
+++ org.agilereview.core/src/org/agilereview/core/external/storage/ReviewModel.java
@@ -1,8 +1,8 @@
 /**
  * Copyright (c) 2011, 2012 AgileReview Development Team and others.
  * All rights reserved. This program and the accompanying materials are made available under
- * the terms of the Apache License v2.0 which accompanies this distribution, and is available
- * at http://www.apache.org/licenses/LICENSE-2.0.html
+ * the terms of the Eclipse Public License - v 1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
  * Contributors: Malte Brunnlieb, Philipp Diebold, Peter Reuter, Thilo Rauch
  */
 package org.agilereview.core.external.storage;
