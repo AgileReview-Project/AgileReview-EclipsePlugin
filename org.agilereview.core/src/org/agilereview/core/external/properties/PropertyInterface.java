54adb744c0ac7468520d6c06e84d54100b301265
Malte Brunnlieb <malte.brunnlieb@agilereview.org>
2012-04-28 12:50:58 +0000

    PropertyInterface added and exported
    git-svn-id: http://svn.code.sf.net/p/agilereview/code/AgileReview_project/branches/new_architecture@915 d30e6b9e-d291-474e-934b-121ad53275c3

---
+++ org.agilereview.core/src/org/agilereview/core/external/properties/PropertyInterface.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2011, 2012 AgileReview Development Team and others.
+ * All rights reserved. This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License - v 1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * Contributors: Malte Brunnlieb, Philipp Diebold, Peter Reuter, Thilo Rauch
+ */
+package org.agilereview.core.external.properties;
+
+/**
+ * 
+ * @author Malte Brunnlieb (28.04.2012)
+ */
+public class PropertyInterface {
+	
+	/**
+	 * Comma separated list of comment status
+	 */
+	public static final String COMMENT_STATUS = "org.agilereview.comment_status";
+	
+}
