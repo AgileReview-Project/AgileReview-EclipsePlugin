9ce86b43789d72a675ee9667fdc937ba51d89e3a
Peter Reuter <peter.reuter@agilereview.org>
2012-06-20 20:55:02 +0000

    added skeleton for reviewset test
    git-svn-id: http://svn.code.sf.net/p/agilereview/code/AgileReview_project/branches/new_architecture@1008 d30e6b9e-d291-474e-934b-121ad53275c3

---
+++ org.agilereview.core.test/src/org/agilereview/core/test/external/storage/ReviewSetTest.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2011, 2012 AgileReview Development Team and others.
+ * All rights reserved. This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License - v 1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * Contributors: Malte Brunnlieb, Philipp Diebold, Peter Reuter, Thilo Rauch
+ */
+package org.agilereview.core.test.external.storage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author Peter Reuter (20.06.2012)
+ */
+public class ReviewSetTest {
+
+	/**
+	 * Test method for {@link org.agilereview.core.external.storage.ReviewSet#clear()}.
+	 */
+	@Test
+	public void testClear() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.agilereview.core.external.storage.ReviewSet#add(org.agilereview.core.external.storage.Review)}.
+	 */
+	@Test
+	public void testAddReview() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.agilereview.core.external.storage.ReviewSet#addAll(java.util.Collection)}.
+	 */
+	@Test
+	public void testAddAllCollectionOfQextendsReview() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.agilereview.core.external.storage.ReviewSet#remove(java.lang.Object)}.
+	 */
+	@Test
+	public void testRemoveObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.agilereview.core.external.storage.ReviewSet#removeAll(java.util.Collection)}.
+	 */
+	@Test
+	public void testRemoveAllCollectionOfQ() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.agilereview.core.external.storage.ReviewSet#retainAll(java.util.Collection)}.
+	 */
+	@Test
+	public void testRetainAllCollectionOfQ() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.agilereview.core.external.storage.ReviewSet#addPropertyChangeListener(java.beans.PropertyChangeListener)}.
+	 */
+	@Test
+	public void testAddPropertyChangeListener() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.agilereview.core.external.storage.ReviewSet#removePropertyChangeListener(java.beans.PropertyChangeListener)}.
+	 */
+	@Test
+	public void testRemovePropertyChangeListener() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.agilereview.core.external.storage.ReviewSet#propertyChange(java.beans.PropertyChangeEvent)}.
+	 */
+	@Test
+	public void testPropertyChange() {
+		fail("Not yet implemented");
+	}
+
+}
