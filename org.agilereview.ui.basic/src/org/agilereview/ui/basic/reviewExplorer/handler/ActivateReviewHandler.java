18b367f2a8b8475cf2dced5c816bd87987b36ad9
Thilo Rauch <thilo.rauch@agilereview.org>
2014-06-23 15:58:48 +0200

    Bugfix: In Eclipse 4.x (e4) the selection provided by HandlerUtil.getCurrentSelection is not always correct --> Use PlatformUITools instead.
    Still to fix: Problems in e4 with activeWhen and enabledWhen. The whole Handler-Selection Infrastructure from Eclipse 3.x seems to be broken in e4. --> Change to e4 infrastructure?

--- org.agilereview.ui.basic/src/org/agilereview/ui/basic/reviewExplorer/handler/ActivateReviewHandler.java
+++ org.agilereview.ui.basic/src/org/agilereview/ui/basic/reviewExplorer/handler/ActivateReviewHandler.java
@@ -1,18 +1,20 @@
 package org.agilereview.ui.basic.reviewExplorer.handler;
 
+import org.agilereview.common.ui.PlatformUITools;
 import org.agilereview.core.external.storage.Review;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.handlers.HandlerUtil;
 
 public class ActivateReviewHandler extends AbstractHandler {
     
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
-        ISelection sel = HandlerUtil.getCurrentSelection(event);
+        // ISelection sel = HandlerUtil.getCurrentSelection(event);
+        // Actually, the code above should be working. But as it sometimes gives the wrong selection in e4, use the line below
+        ISelection sel = PlatformUITools.getActiveWorkbenchPage().getSelection();
         if (sel != null) {
             if (sel instanceof IStructuredSelection) {
                 for (Object o : ((IStructuredSelection) sel).toArray()) {
